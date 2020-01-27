4850f5e19c7ae85192edabb99a45e8cd775a52d8
Volzhanin37 <41235669+Volzhanin37@users.noreply.github.com>
2020-01-27 20:59:04 +0300

    Update 02.py

--- Lesson II/02.py
+++ Lesson II/02.py
@@ -1,1 +1,6 @@
-
+a,b,c = map(int, input('введите 3 числа через пробел: ').split())
+n = 0
+for i in range(a,b):
+    if i%c == 0:
+        n += 1
+print(f"{n} чисел между {a} и {b} делятся на {c}")

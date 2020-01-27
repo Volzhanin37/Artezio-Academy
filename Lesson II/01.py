fcd574f1b744e51e2bdf569427bb11a373990d40
Volzhanin37 <41235669+Volzhanin37@users.noreply.github.com>
2020-01-27 21:31:13 +0300

    Update 01.py

--- Lesson II/01.py
+++ Lesson II/01.py
@@ -1,7 +1,7 @@
-X = int(input('Введите X: '))
-num_of_n = 0
-for n in range(1,X+1,2):
-    print(n*n, end=' ')
-    num_of_n += 1
-print()     
-print("Нечетных чисел в интервале [0:X]:", num_of_n)
+def sq_of_odd(X):
+    num_of_n = 0
+    for n in range(1,X+1,2):
+        print(n*n, end=' ')
+        num_of_n += 1
+    print()     
+    print("Нечетных чисел в интервале [0:X]:", num_of_n)

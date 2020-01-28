178ca5be159d1068fb166742cba9f4825e789a9b
Volzhanin37 <41235669+Volzhanin37@users.noreply.github.com>
2020-01-28 16:06:19 +0300

    Update 04.py

--- Lesson II/04.py
+++ Lesson II/04.py
@@ -1,1 +1,20 @@
+def range2(start=0, stop=None, step=1):
+    if stop == None:
+        stop = start
+        start = 0
+    l = []
+    i = start
+    while ((start < stop and step > 0 and i < stop ) or 
+    (start > stop and step < 0 and i > stop)):  
+        l.append(i)
+        i += step
+    return l
 
+#Test        
+print(range2(0,5,1))   #[0, 1, 2, 3, 4]
+print(range2(0,5))     #[0, 1, 2, 3, 4]
+print(range2(5))       #[0, 1, 2, 3, 4]
+print(range2(4,0,-1))  #[4, 3, 2, 1]
+print(range2(4,0))     #[]
+print(range2(0,5,-1))  #[]  
+print(range2(4,0,1))   #[]  

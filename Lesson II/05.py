53794aa5c2cbc73feacc4dd65ceb8a3c45094f4e
Volzhanin37 <41235669+Volzhanin37@users.noreply.github.com>
2020-01-28 16:07:07 +0300

    Update 05.py

--- Lesson II/05.py
+++ Lesson II/05.py
@@ -1,1 +1,10 @@
-
+password ='qwerty'
+username = input('enter username: ')
+while True:
+    user_password = input('enter password: ')
+    if user_password == password:
+        print(f'Password for user: {username} is correct')
+        break
+    else:
+        print(f'Password for user: {username} is incorrect')
+        print("Please try again...")
